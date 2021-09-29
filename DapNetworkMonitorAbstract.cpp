864c505df4c170332ee067c63b39fbd36baa2c57
Constantin Papizh <p.const@bk.ru>
2021-09-29 17:05:40 +0300

    More accurate monitoring implemented

--- DapNetworkMonitorAbstract.cpp
+++ DapNetworkMonitorAbstract.cpp
@@ -5,36 +5,10 @@ DapNetworkMonitorAbstract::DapNetworkMonitorAbstract(QObject *parent)
     : QObject(parent)
 {
     connect(this, &DapNetworkMonitorAbstract::sigMonitoringStarted, [=]{
-        qDebug() << "Monitoring started";
-        m_isMonitoringRunning = true;
+        m_isMonitoringRunning.store(true);
     });
 
     connect(this, &DapNetworkMonitorAbstract::sigMonitoringFinished, [=]{
-        qDebug() << "Monitoring finished";
-        m_isMonitoringRunning = false;
-    });
-
-    connect(this, &DapNetworkMonitorAbstract::sigTunGatewayDefined, [=]{
-        m_isTunGatewayDefined = true;
-    });
-
-    connect(this, &DapNetworkMonitorAbstract::sigTunGatewayUndefined, [=]{
-        m_isTunGatewayDefined = false;
-    });
-
-    connect(this, &DapNetworkMonitorAbstract::sigOtherGatewayDefined, [=]{
-        m_isOtherGatewayDefined = true;
-    });
-
-    connect(this, &DapNetworkMonitorAbstract::sigOtherGatewayUndefined, [=]{
-        m_isOtherGatewayDefined = false;
-    });
-
-    connect(this, &DapNetworkMonitorAbstract::sigInterfaceDefined, [=]{
-        m_isInterfaceDefined = true;
-    });
-
-    connect(this, &DapNetworkMonitorAbstract::sigInterfaceUndefined, [=]{
-        m_isInterfaceDefined = false;
+        m_isMonitoringRunning.store(false);
     });
 }
--- DapNetworkMonitorWindows.h
+++ DapNetworkMonitorWindows.h
@@ -16,15 +16,11 @@ public:
         return &me;
     }
 
-
     bool isTunDriverInstalled() const override;
     bool monitoringStart() override;
     bool monitoringStop() override;
-
 private:
     void internalWorker();
-    QMutex mutex;
-
     ulong m_TapAdapterIndex = 0, m_DefaultAdapterIndex = 0;
     static void cbRouteChanged(void *ctx, PMIB_IPFORWARD_ROW2 route, MIB_NOTIFICATION_TYPE type);
     static void cbIfaceChanged(void *ctx, PMIB_IPINTERFACE_ROW row, MIB_NOTIFICATION_TYPE type);
@@ -33,6 +29,7 @@ signals:
 public slots:
     void sltSetTapIfIndex   (const ulong index) { m_TapAdapterIndex = index; }
     void sltSetIfIndex      (const ulong index) { m_DefaultAdapterIndex = index; }
+    void procErr(const int, const QString&);
 };
 
 #endif // NETWORKMONITORWINDOWS_H
