d5c9681f06852852cd7f31e1092321faf4fc4f22
danil.martynenko <danil.martynenko@demlabs.net>
2024-07-09 18:18:49 +0300

    [*] linux network monitor refactoring

--- DapNetworkMonitorLinux.cpp
+++ DapNetworkMonitorLinux.cpp
@@ -2,104 +2,120 @@
 
 void DapNetworkMonitorLinux::cbMonitorNotification(const dap_network_notification_t notification)
 {
-    if(notification.type == IP_ADDR_ADD || notification.type == IP_ADDR_REMOVE) {
+    auto instance = DapNetworkMonitorLinux::instance();
 
+    switch(notification.type) {
+    case IP_ADDR_ADD:
+    case IP_ADDR_REMOVE: {
         qInfo() << QString("Interface %1 %2 has IP address %3")
-                    .arg(notification.addr.interface_name)
-                    .arg((notification.type == IP_ADDR_ADD ? "now" : "no longer"))
-                    .arg(notification.addr.s_ip);
+                       .arg(notification.addr.interface_name)
+                       .arg((notification.type == IP_ADDR_ADD ? "now" : "no longer"))
+                       .arg(notification.addr.s_ip);
 
         if(notification.type == IP_ADDR_ADD)
-            emit instance()->sigInterfaceDefined();
+            emit instance->sigInterfaceDefined();
         else
-            emit instance()->sigInterfaceUndefined();
+            emit instance->sigInterfaceUndefined();
+        break;
+    }
 
-    } else if(notification.type == IP_ROUTE_ADD || notification.type == IP_ROUTE_REMOVE) {
-        emit instance()->sigRouteChanged();
+    case IP_ROUTE_ADD:
+    case IP_ROUTE_REMOVE: {
+        emit instance->sigRouteChanged();
 
         qDebug() << QString("%1 route to destination --> %2/%3 proto %4 and gateway %5")
-                    .arg((notification.type == IP_ROUTE_ADD ? "Add" : "Delete"))
-                    .arg(notification.route.s_destination_address)
-                    .arg(notification.route.netmask)
-                    .arg(notification.route.protocol)
-                    .arg(notification.route.s_gateway_address);
+                        .arg((notification.type == IP_ROUTE_ADD ? "Add" : "Delete"))
+                        .arg(notification.route.s_destination_address)
+                        .arg(notification.route.netmask)
+                        .arg(notification.route.protocol)
+                        .arg(notification.route.s_gateway_address);
 
         if (notification.type == IP_ROUTE_REMOVE) {
             if(notification.route.destination_address == DAP_ADRESS_UNDEFINED &&
-                    notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
+                notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
+
                 QString gatewayAddr(notification.route.s_gateway_address);
-                if(gatewayAddr == instance()->m_tunnelGateway) {
-                    if (checkTunnelGw()){
+                if(gatewayAddr == instance->m_tunnelGateway) {
+                    if (checkTunnelGw()) {
                         qInfo() << "Tunnel gateway is still defined";
-                        emit instance()->sigTunGatewayDefined();
+                        emit instance->sigTunGatewayDefined();
                     } else {
                         qInfo() << "Tunnel gateway is undefined";
-                        emit instance()->sigTunGatewayUndefined();
+                        emit instance->sigTunGatewayUndefined();
                     }
                 } else {
                     qInfo() << "Other gateway is undefined";
-                    emit instance()->sigOtherGatewayUndefined();
+                    emit instance->sigOtherGatewayUndefined();
                 }
             } else if(notification.route.destination_address != DAP_ADRESS_UNDEFINED &&
-                      notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
-                if(instance()->isUpstreamRoute(notification.route.s_destination_address,
-                                               notification.route.s_gateway_address)) {
+                       notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
+                if(instance->isUpstreamRoute(notification.route.s_destination_address,
+                                              notification.route.s_gateway_address)) {
                     qInfo() << "Upstream route is undefined";
-                    emit instance()->sigUpstreamRouteUndefined();
+                    emit instance->sigUpstreamRouteUndefined();
                 }
             }
-        } else  if (notification.type == IP_ROUTE_ADD) {
+        } else if (notification.type == IP_ROUTE_ADD) {
             if(notification.route.destination_address == DAP_ADRESS_UNDEFINED &&
-                    notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
+                notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
                 QString gatewayAddr(notification.route.s_gateway_address);
 
-                if(gatewayAddr == instance()->m_tunnelGateway) {
+                if(gatewayAddr == instance->m_tunnelGateway) {
                     qInfo() << "Tunnel gateway is defined";
-                    emit instance()->sigTunGatewayDefined();
+                    emit instance->sigTunGatewayDefined();
                 } else {
                     qInfo() << "Other gateway is defined";
-                    emit instance()->sigOtherGatewayDefined(gatewayAddr);
+                    emit instance->sigOtherGatewayDefined(gatewayAddr);
                 }
             } else if(notification.route.destination_address != DAP_ADRESS_UNDEFINED &&
-                      notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
-                if(instance()->isUpstreamRoute(notification.route.s_destination_address,
-                                               notification.route.s_gateway_address)) {
+                       notification.route.gateway_address != DAP_ADRESS_UNDEFINED) {
+                if(instance->isUpstreamRoute(notification.route.s_destination_address,
+                                              notification.route.s_gateway_address)) {
                     qInfo() << "Upstream route is defined";
-                    emit instance()->sigUpstreamRouteDefined();
+                    emit instance->sigUpstreamRouteDefined();
                 }
             }
         }
-    } else if(notification.type == IP_LINK_NEW || notification.type == IP_LINK_DEL){
-        qInfo() << QString("Interface %1 is %2 %3 %4")
-                    .arg(notification.link.interface_name)
-                    .arg(( (notification.type == IP_LINK_NEW && notification.link.is_up) ? "UP" : "DOWN") )
-                    .arg((notification.type == IP_LINK_NEW ? "and" : ""))
-                    .arg((notification.type == IP_LINK_NEW ?
-                         (notification.link.is_running ? "RUNNING" : "NOT RUNNING")
-                            : ""));
-
-        if(/*(notification.type == IP_LINK_NEW && !notification.link.is_running) || */notification.type == IP_LINK_DEL)
-            emit instance()->sigInterfaceUndefined();
-        else
-            emit instance()->sigInterfaceDefined();
+        break;
     }
 
+    case IP_LINK_NEW:
+    case IP_LINK_DEL: {
+        qInfo() << QString("Interface %1 is %2 %3 %4")
+                       .arg(notification.link.interface_name)
+                       .arg(((notification.type == IP_LINK_NEW && notification.link.is_up) ? "UP" : "DOWN"))
+                       .arg((notification.type == IP_LINK_NEW ? "and" : ""))
+                       .arg((notification.type == IP_LINK_NEW ?
+                                 (notification.link.is_running ? "RUNNING" : "NOT RUNNING")
+                                                              : ""));
+
+        if(notification.type == IP_LINK_DEL)
+            emit instance->sigInterfaceUndefined();
+        else
+            emit instance->sigInterfaceDefined();
+        break;
+    }
+
+    default:
+        qWarning() << "Unknown notification type received";
+    }
 }
 
 bool DapNetworkMonitorLinux::checkTunnelGw()
 {
+    auto instance = DapNetworkMonitorLinux::instance();
+
     int ret;
-    if(instance()->m_tunnelGateway.size() > 0){
-        ret =  ::system(QString("netstat -rn | grep 'UG ' | grep %1 > /dev/null ").arg(instance()->m_tunnelGateway)
-                        .toLatin1().constData());
-        if(ret == 0 ){
-            return true;
-        }
+    if(instance->m_tunnelGateway.size() > 0) {
+        ret = ::system(QString("netstat -rn | grep 'UG ' | grep %1 > /dev/null ")
+                           .arg(instance->m_tunnelGateway)
+                           .toLatin1().constData());
+        return (ret == 0);
     }
     return false;
 }
 
-DapNetworkMonitorLinux::DapNetworkMonitorLinux(QObject *parent):
+DapNetworkMonitorLinux::DapNetworkMonitorLinux(QObject *parent) :
     DapNetworkMonitorAbstract(parent)
 {
     m_isTunGatewayDefined.store(true);
@@ -116,8 +132,9 @@ bool DapNetworkMonitorLinux::isTunDriverInstalled() const
 bool DapNetworkMonitorLinux::monitoringStart()
 {
     qDebug() << "Start network monitoring";
-    if(m_isMonitoringRunning == true) {
+    if(m_isMonitoringRunning) {
         qWarning() << "Network monitoring already running";
+        return true;
     }
 
     if(dap_network_monitor_init(cbMonitorNotification) == 0) {
@@ -131,7 +148,6 @@ bool DapNetworkMonitorLinux::monitoringStop()
 {
     qDebug() << "Stop network monitoring";
     dap_network_monitor_deinit();
-
     m_isMonitoringRunning = false;
     return true;
 }
--- DapNetworkMonitorLinux.h
+++ DapNetworkMonitorLinux.h
@@ -2,13 +2,14 @@
 #include "DapNetworkMonitorAbstract.h"
 #include "DapMonitorCmdProgram.h"
 #include "dap_network_monitor.h"
-
+#include <QString>
+#include <atomic>
 
 class DapNetworkMonitorLinux : public DapNetworkMonitorAbstract
 {
     Q_OBJECT
-private:
 
+private:
     static void cbMonitorNotification(const dap_network_notification_t notification);
     static bool checkTunnelGw();
 
@@ -16,11 +17,18 @@ private:
     DapNetworkMonitorLinux(const DapNetworkMonitorLinux&) = delete;
     DapNetworkMonitorLinux& operator=(const DapNetworkMonitorLinux&) = delete;
 
+    QString m_tunnelGateway;
+    std::atomic<bool> m_isMonitoringRunning;
+
 public:
     static DapNetworkMonitorLinux* instance()
-        {static DapNetworkMonitorLinux client; return &client;}
+    { static DapNetworkMonitorLinux client; return &client; }
 
     bool isTunDriverInstalled() const override;
+
+    void setTunnelGateway(const QString &gateway) { m_tunnelGateway = gateway; }
+    QString tunnelGateway() const { return m_tunnelGateway; }
+
 signals:
 
 public slots:
